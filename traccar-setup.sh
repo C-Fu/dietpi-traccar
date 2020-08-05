7771187efb1711ad35148fc0e7bde4b1571a77d8
Adri Idzwan Mansor <adri.mcmc@gmail.com>
2020-08-06 06:30:12 +0800

    Update traccar-setup.sh

--- traccar-setup.sh
+++ traccar-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /root/traccar
 cd /root/traccar
-curl https://github.com/traccar/traccar/releases/download/v4.10/traccar-linux-arm-4.10.zip
-unzip traccar-linux-arm-4.10.zip
+curl -L https://github.com/traccar/traccar/releases/download/v4.10/traccar-linux-arm-4.10.zip > traccar.zip
+unzip traccar.zip
 ./traccar.run
 systemctl start traccar
