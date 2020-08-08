ddd8973ba69f8ccba634ceac233ff4ae3cea8550
Adri Idzwan Mansor <adri.mcmc@gmail.com>
2020-08-09 04:13:38 +0800

    Update traccar-setup.sh

--- traccar-setup.sh
+++ traccar-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install unzip
+apt install unzip rsync certbot python3-certbot-apache python-smbus i2c-tools
 mkdir /root/traccar
 cd /root/traccar
 curl -L https://github.com/traccar/traccar/releases/download/v4.10/traccar-linux-arm-4.10.zip > traccar.zip
